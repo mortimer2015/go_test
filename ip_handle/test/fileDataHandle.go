5b2b8c5f6047970f9c6ae6b1c58ca4d1e36ae36b
hunter <328624847@qq.com>
2020-04-24 18:08:31 +0800

    日志解析功能测试

--- ip_handle/test/fileDataHandle.go
+++ ip_handle/test/fileDataHandle.go
@@ -3,24 +3,26 @@ package main
 import (
 	"fmt"
 	"go_test/ip_handle/ipDataHandle"
-	"io/ioutil"
-	"strings"
+	"time"
 )
 
 func main() {
+	fmt.Println(time.Now())
 	ipList, ipDictData := ipDataHandle.IpDict("file/ipdata.txt")
-	//ret := ipDataHandle.IpFrom(ipList, ipDictData, "1.0.1.222")
-	fileName := "file/test.csv"
-	f, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		fmt.Println(err)
-	}
-	data := strings.Split(string(f), "\n")
-	for i := 0; i < len(data); i++ {
-		//fmt.Println(data[i])
-		line := strings.Split(data[i], ", ")
-		//fmt.Println(line[len(line)-1])
-		ret := ipDataHandle.IpFrom(ipList, ipDictData, line[3])
-		fmt.Printf("%s, %s\n", data[i], ret)
-	}
+	ret := ipDataHandle.IpFrom(ipList, ipDictData, "1.0.1.222")
+	fmt.Printf(ret)
+	//fileName := "file/test.csv"
+	//f, err := ioutil.ReadFile(fileName)
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//data := strings.Split(string(f), "\n")
+	//for i := 0; i < len(data); i++ {
+	//	//fmt.Println(data[i])
+	//	line := strings.Split(data[i], ", ")
+	//	//fmt.Println(line[len(line)-1])
+	//	ret := ipDataHandle.IpFrom(ipList, ipDictData, line[3])
+	//	fmt.Printf("%s, %s", data[i], ret)
+	//}
+	fmt.Println(time.Now())
 }
